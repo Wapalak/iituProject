ce6b0fcedfd54bb8340bae74703192f91364534b
Wapalak <jamba142004@gmail.com>
2023-12-08 13:16:24 +0600

    123

--- iituproject/main/views.py
+++ iituproject/main/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Success<h1>")
+    return render(request, 'main/index.html')
 
 def about(request):
-    return HttpResponse("<h1>Z O V<h1>")
+    return render(request, 'main/about.html')
