9110dc7982b6e85ff4201a74ccef807414a809dc
Wapalak <jamba142004@gmail.com>
2023-12-08 17:48:33 +0600

    123

--- iituproject/news/models.py
+++ iituproject/news/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Articles(models.Model):
+    title = models.CharField('Название', max_length=50)
+    review = models.CharField('Отзыв', max_length=200)
+    full_text = models.TextField('Текст')
+    date = models.DateTimeField('Дата публикации')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'New table'
+        verbose_name_plural = 'New tables'
